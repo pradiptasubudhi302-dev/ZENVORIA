c387c43dc68c3af24aa74bff45abfe95d4662982
pradiptasubudhi302-dev <pradiptasubudhi302@gmail.com>
2026-07-06 14:17:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-const btn = document.getElementById("exploreBtn");
+const exploreBtn = document.getElementById("exploreBtn");
 
-btn.addEventListener("click", () => {
-  alert("🚀 Welcome to ZENVORIA!\nLet's build something amazing together.");
-});
-
-window.addEventListener("scroll", () => {
-  const navbar = document.querySelector(".navbar");
-
-  if (window.scrollY > 50) {
-    navbar.style.background = "rgba(15,23,42,0.95)";
-  } else {
-    navbar.style.background = "rgba(0,0,0,0.35)";
-  }
+exploreBtn.addEventListener("click", () => {
+    alert("Welcome to ZENVORIA! 🚀");
+    document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    });
 });
